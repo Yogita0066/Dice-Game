d2bb74d1144a571448fc506d15c92fb246348f5e
Yogita <yogitasingh904@gmail.com>
2023-08-09 15:52:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber = Math.floor(Math.random()* 6)+1;
+var randomDiceImage = "dice" + randomNumber + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber1 = Math.floor(Math.random()* 6)+1;
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
+
+
+if(randomNumber > randomNumber1){
+    document.querySelector("h1").innerHTML = "1 Wins!";
+}
+else if(randomNumber1 > randomNumber){
+    document.querySelector("h1").innerHTML = "2 wins1";
+}
+else{
+    document.querySelector("h1").innerHTML = "draw!";
+}
